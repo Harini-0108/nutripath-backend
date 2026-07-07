af0de102437483209375c864c96561dae3bdd991
Harini-0108 <harini48823@gmail.com>
2026-07-07 17:00:29 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/nutripath/Security/SecurityConfig.java
+++ src/main/java/com/example/nutripath/Security/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.nutripath.Security;
 
 import java.util.List;
-
+import org.springframework.security.config.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
